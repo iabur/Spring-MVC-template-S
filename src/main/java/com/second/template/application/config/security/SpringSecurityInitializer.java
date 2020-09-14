0805660eca77d28f57e116a95c5d50a221762edb
iaburrahman <iaburrahman@gmail.com>
2020-09-14 12:26:25 +0600

    setup

---
+++ src/main/java/com/second/template/application/config/security/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.second.template.application.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
---
+++ src/main/java/com/second/template/application/util/Constants.java
@@ -0,0 +1,6 @@
+package com.second.template.application.util;
+
+public  class Constants
+{
+	public static final String UPLOADED_FOLDER  = "/WEB-INF/resources/img/";
+}
