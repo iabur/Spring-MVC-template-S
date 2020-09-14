d938f1be27a96afbe7e5065a6b8747aff9ca18ee
iaburrahman <iaburrahman@gmail.com>
2020-09-14 11:04:58 +0600

    update

--- template 2/src/main/java/com/second/template/application/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.second.template.application.util;
-
-public  class Constants
-{
-	public static final String UPLOADED_FOLDER  = "/WEB-INF/resources/img/";
-}
--- template 2/target/This_is_template2-0.0.1-SNAPSHOT/WEB-INF/resources/js/sample.js
+++
@@ -1,5 +0,0 @@
-function doSomeWork() {
-	
-	alert("wow js is working in spring mvc....");
-	
-}
